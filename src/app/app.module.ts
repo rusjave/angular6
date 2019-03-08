909f8c0e24a143939ca2e86d5327cbffcece9417
Russell Jave Rivera <rjrivera@offsure.local>
2019-03-08 14:58:25 +0800

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,  routingComponents } from './app-routing.module';
+
+//import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { DeparmentListComponent } from './deparment-list/deparment-list.component';
+// import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { from } from 'rxjs';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DepartmentDetailComponent } from './department-detail/department-detail.component';
+import { DepartmentOverviewComponent } from './department-overview/department-overview.component';
+import { DepartmentContactComponent } from './department-contact/department-contact.component';
+// import { DeparmentListComponent } from './deparment-list/deparment-list.component';
+// import { EmployeeListComponent } from './employee-list/employee-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+    PageNotFoundComponent,
+    DepartmentDetailComponent,
+    DepartmentOverviewComponent,
+    DepartmentContactComponent
   ],
   imports: [
     BrowserModule,
